f456f9db32b8c90c8a0a45525423924c8a03eb1c
Kern84 <adrienleroy1984@gmail.com>
2022-07-21 10:36:14 +0200

    update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,11 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://oc-lettings-application.herokuapp.com/", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "https://oc-lettings-application.herokuapp.com/",
+    "127.0.0.1",
+    "oc-lettings-application.herokuapp.com",
+]
 
 
 # Application definition
